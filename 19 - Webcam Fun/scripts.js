b4ebc28c0db4aabe441ea495d596858dc1610ce9
Kaho Shibuya <51708229+shiv-chan@users.noreply.github.com>
2021-03-22 17:24:45 +0900

    Update scripts.js

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,73 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+  .then(localMediaStream => {
+    console.log(localMediaStream);
+    video.srcObject = localMediaStream;
+    video.play();
+  })
+  .catch((err) => {
+    console.error(`Oh No!`, err);
+  });
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    
+    //take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+
+    //mess with them
+    // pixels = redEffect(pixels);
+    pixels = rgbSplit(pixels);
+    ctx.globalAlpha = 0.1
+
+    //put them back
+    ctx.putImageData(pixels, 0, 0);
+  }, 16)
+}
+
+function takePhoto() {
+  //play the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  //take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute ('download', 'photo');
+  link.innerHTML = `<img src = "${data}" alt="photo">`
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for(let i = 0; i < pixels.data.length; i += 4){
+    pixels.data[i] += 100 //red
+    pixels.data[i + 1] -= 50//green
+    pixels.data[i + 2] *= 0.5//blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for(let i = 0; i < pixels.data.length; i += 4){
+    pixels.data[i - 150] = pixels.data[i] //red
+    pixels.data[i + 100] = pixels.data[i + 1]//green
+    pixels.data[i - 550] = pixels.data[i + 2]//blue
+  }
+  return pixels;
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
